87dd92f52d4e562cbdfaa5b63f36d94329c8a399
angel_ivanov <angelspsv@gmail.com>
2024-03-08 17:27:00 +0100

    fitxer html creats

--- TIC_BCN_AIS/centre/views.py
+++ TIC_BCN_AIS/centre/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello world!")
+
+def index(request):
+    template = loader.get_template('alumnat.html')
+    return HttpResponse(template.render())
