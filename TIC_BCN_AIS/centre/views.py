406c4525aac2b9b4c69a998cf93354779d0694f1
angel_ivanov <angelspsv@gmail.com>
2024-03-14 17:42:14 +0100

    modificando y adaptando los archivos html

--- TIC_BCN_AIS/centre/views.py
+++ TIC_BCN_AIS/centre/views.py
@@ -12,13 +12,23 @@ def students(request):
     return render(request, 'alumnat.html', {'students': students})
 
 def student(request, pk):
-    students = {"nom": "Angel", "cognom": "Ivanov", "edat": "16", "rol": "alumne(TOP)", "curs": "DAW+"}
-    return render(request, 'alumne.html', {'alumne': students})
+    students = [
+        {"nom": "Angel", "cognom": "Ivanov", "edat": "38", "rol": "alumne", "curs": "DAW1, DAW2A"},
+        {"nom": "Ramon", "cognom": "Garcia", "edat": "25", "rol": "alumne", "curs": "DAW2A"},
+        {"nom": "Laura", "cognom": "Fernandez", "edat": "24", "rol": "alumne", "curs": "DAW2A"}
+    ]
+    student = students[int(pk)-1]
+    return render(request, 'alumne.html', {'alumne': student})
 
 def teachers(request):
     teachers = "Teacher.objects.all()"
     return render(request, 'professorat.html', {'teachers': teachers})
 
 def teacher(request, pk):
-    professor = {"nom": "Roger", "cognom": "Sobrino", "edat": "17", "rol": "teacher", "curs": "DAW8"}
-    return render(request, 'professor.html', {'professor': professor})
+    teachers = [
+        {"nom": "Roger", "cognom": "Sobrino", "edat": "39", "rol": "teacher", "curs": "DAM2B, DAW2A"},
+        {"nom": "Pere", "cognom": "Guitart", "edat": "57", "rol": "teacher", "curs": "DAW1"},
+        {"nom": "Juanma", "cognom": "Biel", "edat": "50", "rol": "teacher", "curs": "DAW2B, DAW2A"}
+    ]
+    teacher = teachers[int(pk)-1]
+    return render(request, 'professor.html', {'professor': teacher})
