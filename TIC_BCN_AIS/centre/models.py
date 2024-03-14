b4a53a66028b80322c85cd18c3fd9359ba7a8f23
angel_ivanov <angelspsv@gmail.com>
2024-03-14 23:58:18 +0100

    creats els camps de la taula a models.py

--- TIC_BCN_AIS/centre/models.py
+++ TIC_BCN_AIS/centre/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    ALUMNE = 'alumne'
+    PROFESSOR = 'professor'
+    ROL_CHOICES = [
+        (ALUMNE, 'Alumne'),
+        (PROFESSOR, 'Professor'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=20)
+    cognom = models.CharField(max_length=30)
+    rol = models.CharField(max_length=10, choices=ROL_CHOICES, default=ALUMNE)
+    email = models.EmailField(max_length=254)
+    edat = models.IntegerField()
+    sexe = models.CharField(max_length=1, choices=(('H', 'Hombre'), ('M', 'Mujer')))
+
+    def __str__(self):
+        return f"ID: {self.id}, Nom: {self.nom}, Cognom: {self.cognom}, Rol: {self.rol}, Email: {self.email}, Edat: {self.edat}, Sexe: {self.sexe}"
