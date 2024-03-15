bfbf011d975dc0bbaf17b4aa5b96b2adefbec2b7
angel_ivanov <angelspsv@gmail.com>
2024-03-15 08:51:07 +0100

    modificats alguns dels camps de la taula a models.py

--- TIC_BCN_AIS/centre/models.py
+++ TIC_BCN_AIS/centre/models.py
@@ -1,20 +1,13 @@
 from django.db import models
 
 class Persona(models.Model):
-    ALUMNE = 'alumne'
-    PROFESSOR = 'professor'
-    ROL_CHOICES = [
-        (ALUMNE, 'Alumne'),
-        (PROFESSOR, 'Professor'),
-    ]
-
-    id = models.AutoField(primary_key=True)
+    # el camp id no esta perque ho afegira Django amb la migracio
     nom = models.CharField(max_length=20)
     cognom = models.CharField(max_length=30)
-    rol = models.CharField(max_length=10, choices=ROL_CHOICES, default=ALUMNE)
-    email = models.EmailField(max_length=254)
     edat = models.IntegerField()
-    sexe = models.CharField(max_length=1, choices=(('H', 'Hombre'), ('M', 'Mujer')))
+    rol = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    telefon = models.CharField(max_length=20)
 
     def __str__(self):
-        return f"ID: {self.id}, Nom: {self.nom}, Cognom: {self.cognom}, Rol: {self.rol}, Email: {self.email}, Edat: {self.edat}, Sexe: {self.sexe}"
+        return f"ID: {self.id}, Nom: {self.nom}, Cognom: {self.cognom}, Edat: {self.edat}, Rol: {self.rol}, Email: {self.email}, Telefon: {self.telefon}"
