e6f75ea5a877634f46e5f8b7c099207df08da246
angel_ivanov <angelspsv@gmail.com>
2024-03-21 19:57:14 +0100

    fets: user_form, path, view

--- TIC_BCN_AIS/centre/models.py
+++ TIC_BCN_AIS/centre/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    # el camp ID no esta perque ho afegira Django amb la migracio
+    nom = models.CharField(max_length=20)
+    cognom = models.CharField(max_length=30)
+    edat = models.IntegerField()
+    rol = models.CharField(max_length=10)
+    email = models.EmailField(max_length=90)
+    telefon = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"ID: {self.id}, Nom: {self.nom}, Cognom: {self.cognom}, Edat: {self.edat}, Rol: {self.rol}, Email: {self.email}, Telefon: {self.telefon}"
